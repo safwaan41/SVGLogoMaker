ad5beb1f92114dedc091c9a024ce363da9273fab
safwaan41 <safwaan41@gmail.com>
2023-07-18 22:34:58 -0500

    added user prompt questions, made classes for shapes

---
+++ index.js
@@ -0,0 +1,29 @@
+const inquirer = require (`inquirer`);
+const fs = require (`fs`);
+
+const questions = [
+    // questions to prompt user for desired output 
+    {
+    name: `letters`,
+    type: `input`,
+    message: `Which characters would you like to display? (upto 3)`,
+    validate: (input) => input.length <= 3 // WILL NOT LET USER INPUT MORE THAN 3 CHARACTERS IN COMMAND PROMPT
+    }
+    ,
+    {
+        name: `shape`,
+        type: `list`,
+        message: `Please pick which shape you would like your logo to be...`,
+        choices: [`Square`, `Circle`, `Triangle`]
+    }
+    ,
+    {
+        name: `color`,
+        type: `input`,
+        message: `What color would you like your logo to be? (You can also enter hex color codes)`
+    }
+]
+inquirer.prompt(questions)
+.then(answers =>{
+    fs.writeFile(`./examples/output.svg`,)
+});
